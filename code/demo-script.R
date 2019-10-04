3cb3f31e25a436c2385d9c8860c3a3cf4b72381f
Alice Young <aliceyoung@berkeley.edu>
2019-10-03 19:54:49 -0700

    code: add demo script

---
+++ code/demo-script.R
@@ -0,0 +1,15 @@
+# ===================================================================
+# Title: Descriptive Analysis
+# Description:
+#   This script computes descriptive statistics, as well as 
+#   various exploratory data visualizations.
+# Input(s): data file 'nba2018-players.csv'
+# Output(s): summary data files, and plots
+# Author: Alice Young
+# Date: 10-3-2019
+# ===================================================================
+
+# packages
+library(dplyr)
+library(ggplot2)
+
